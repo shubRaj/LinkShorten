f7944156c3755d65c345dabb28cf0eb2e2f451b3
Shub Raj Lama <shuvraj1234@gmail.com>
2021-02-02 02:56:36 +0545

    autocommit

--- manage.py
+++ manage.py
@@ -19,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()#test2#test4#test6#test8#test10#test12#test14#test16#test18#test20#test22#test24#test26
+    main()#test2#test4#test6#test8#test10#test12#test14#test16#test18#test20#test22#test24#test26#test
