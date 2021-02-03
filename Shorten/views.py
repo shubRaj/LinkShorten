fcc5fff637b6d013ea130e0bc2d501272c879f54
Shub Raj Lama <shuvraj1234@gmail.com>
2021-02-03 23:11:39 +0545

    Update views.py

--- Shorten/views.py
+++ Shorten/views.py
@@ -12,7 +12,7 @@ from .models import Tracker,Shorten,Log
 def find_ip(request):
     x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
-        ip = x_forwarded_for.split(",")[-1]
+        ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get("REMOTE_ADDR")
     return ip
